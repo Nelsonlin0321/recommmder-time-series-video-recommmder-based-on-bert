56f84d013bb3d84b5f315ff61ce9d1ee589447fb
Nelsonlin0321 <nlin@glgroup.com>
2022-08-22 11:39:26 +0800

    update model

--- model.py
+++ model.py
@@ -116,12 +116,6 @@ class VedioRecommender(AlbertPreTrainedModel):
         self,
         inputs:Optional[dict]=None,
     ) -> Union[SequenceClassifierOutput, Tuple]:
-        r"""
-        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
-            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
-            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
-            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
-        """
 
         outputs = self.albert(
             input_ids=inputs["input_ids"],
